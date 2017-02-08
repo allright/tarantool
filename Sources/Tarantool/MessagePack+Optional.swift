f1a920246ed25f72f63454a688f8f7a2f1426502
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-02-08 05:25:34 +0000

    Add Optional<MessagePack> initializers for primitive types

--- Sources/Tarantool/MessagePack+Optional.swift
+++ Sources/Tarantool/MessagePack+Optional.swift
@@ -10,6 +10,34 @@
 
 import MessagePack
 
+protocol MessagePackInitializable {}
+
+extension Bool: MessagePackInitializable {}
+extension String: MessagePackInitializable {}
+extension Float: MessagePackInitializable {}
+extension Double: MessagePackInitializable {}
+extension Int: MessagePackInitializable {}
+extension Int8: MessagePackInitializable {}
+extension Int16: MessagePackInitializable {}
+extension Int32: MessagePackInitializable {}
+extension Int64: MessagePackInitializable {}
+extension UInt: MessagePackInitializable {}
+extension UInt8: MessagePackInitializable {}
+extension UInt16: MessagePackInitializable {}
+extension UInt32: MessagePackInitializable {}
+extension UInt64: MessagePackInitializable {}
+
+extension MessagePackInitializable {
+    public init?(_ optional: MessagePack?) {
+        guard case let .some(some) = optional,
+            let value = Self(some) else {
+                return nil
+        }
+
+        self = value
+    }
+}
+
 extension Sequence where Iterator.Element == MessagePack {
     public init?(_ optional: MessagePack?) {
         guard case let .some(some) = optional,
