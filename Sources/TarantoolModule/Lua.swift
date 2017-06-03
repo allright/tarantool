d2e5ca7e7a265ffe9a31661dcab6ddaf40cb4fab
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-06-03 13:58:57 +0000

    Read lua error message from the stack

--- Sources/TarantoolModule/Lua.swift
+++ Sources/TarantoolModule/Lua.swift
@@ -50,7 +50,8 @@ public struct Lua {
             _luaL_checkstack(L, Int32(arguments.count), "eval: out of stack")
             try push(values: arguments, to: L)
             guard _luaT_call(L, Int32(arguments.count), LUA_MULTRET) == 0 else {
-                throw LuaError(message: "eval: failed")
+                let pointer = _lua_tolstring(L, -1, nil)!
+                throw LuaError(message: String(cString: pointer))
             }
             return try popValues(from: L)
         }
