fce609bece34aa4cdd8c424397f34f2eb554ee21
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-11-11 23:29:40 +0000

    Optimize ChapSha1 a little

--- Sources/TarantoolConnector/ChapSha1.swift
+++ Sources/TarantoolConnector/ChapSha1.swift
@@ -15,18 +15,16 @@ extension Array where Element == UInt8 {
     func chapSha1(salt: [UInt8]) -> [UInt8] {
         let scrambleSize = 20
 
-        var step1 = [UInt8](repeating: 0, count: scrambleSize)
-        var step2 = [UInt8](repeating: 0, count: scrambleSize)
-        var step3 = [UInt8](repeating: 0, count: scrambleSize)
-        var scramble = [UInt8](repeating: 0, count: scrambleSize)
+        let salt = salt.prefix(upTo: scrambleSize)
 
-        step1 = self.sha1()
-        step2 = step1.sha1()
-        step3 = (salt.prefix(upTo: scrambleSize) + step2).sha1()
+        var step1 = self.sha1()
+        let step2 = step1.sha1()
+        let step3 = (salt + step2).sha1()
 
         for i in 0 ..< scrambleSize {
-            scramble[i] = step1[i] ^ step3[i]
+            step1[i] ^= step3[i]
         }
-        return scramble
+
+        return step1
     }
 }
