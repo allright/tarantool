18ecd77fedaa482d5642ea29ed6c48f4ea408d9f
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-01-14 22:50:10 +0000

    [Tests] Generate LinuxMain.swift

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,12 +1,12 @@
 import XCTest
 
-import TarantoolModuleTests
 import TarantoolConnectorTests
+import TarantoolModuleTests
 import TestUtilsTests
 
 var tests = [XCTestCaseEntry]()
-tests += TarantoolModuleTests.__allTests()
 tests += TarantoolConnectorTests.__allTests()
+tests += TarantoolModuleTests.__allTests()
 tests += TestUtilsTests.__allTests()
 
 XCTMain(tests)
